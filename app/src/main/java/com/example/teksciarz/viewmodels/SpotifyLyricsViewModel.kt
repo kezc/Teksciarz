85d4f903e39c1ff90dff4ecee04ce0fe2a30b564
wojtek <krkezc@gmail.com>
2020-08-07 14:49:59 +0200

    fix when cant get song

--- app/src/main/java/com/example/teksciarz/viewmodels/SpotifyLyricsViewModel.kt
+++ app/src/main/java/com/example/teksciarz/viewmodels/SpotifyLyricsViewModel.kt
@@ -14,6 +14,7 @@ import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import com.spotify.protocol.types.Track
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 private const val TAG = "SpotifyLyricsViewModel"
 private const val CLIENT_ID = "a7dc1dd3f7e24e4e9b7fd4a7b7ed93bd"
@@ -88,14 +89,18 @@ class SpotifyLyricsViewModel(application: Application) : AndroidViewModel(applic
                         "$title by $artist"
                     )
                     viewModelScope.launch {
-                        val song = rep.getSongByArtistAndTitle(artist, title)
-                        _currentSong.value = song
+                        try {
+                            val song = rep.getSongByArtistAndTitle(artist, title)
+                            _currentSong.value = song
+                        } catch (e: IOException) {
+                            Log.d(TAG, "Network exception")
+                        }
                     }
                 }
             }
     }
 
-    fun disconnectFromSpotify() {
+    private fun disconnectFromSpotify() {
         Log.d(TAG, "Disconnecting from Spotify")
         spotifyAppRemote?.let {
             SpotifyAppRemote.disconnect(it)
