ae114d53e6695e4747bacb6483e6ffb9f0cad1d2
wojtek <krkezc@gmail.com>
2020-09-26 13:54:27 +0200

    connection status bar

--- app/src/main/java/com/example/teksciarz/SpotifyLyricsFragment.kt
+++ app/src/main/java/com/example/teksciarz/SpotifyLyricsFragment.kt
@@ -1,7 +1,11 @@
 package com.example.teksciarz
 
 import android.app.AlertDialog
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Bundle
 import android.text.Spannable
@@ -16,6 +20,7 @@ import androidx.annotation.ColorRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import androidx.transition.Fade
 import androidx.transition.TransitionManager
@@ -23,6 +28,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.teksciarz.viewmodels.SpotifyLyricsViewModel
 import com.google.android.material.snackbar.Snackbar
+import com.spotify.android.appremote.api.error.AuthenticationFailedException
 import com.spotify.android.appremote.api.error.CouldNotFindSpotifyApp
 import com.spotify.android.appremote.api.error.NotLoggedInException
 import com.spotify.android.appremote.api.error.UserNotAuthorizedException
@@ -30,6 +36,10 @@ import kotlinx.android.synthetic.main.fragment_spotify_lyrics.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 @InternalCoroutinesApi
@@ -46,16 +56,16 @@ class SpotifyLyricsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var snackbar: Snackbar? = null
 
         val circularProgressDrawable = CircularProgressDrawable(requireContext())
         circularProgressDrawable.strokeWidth = 5f
         circularProgressDrawable.centerRadius = 30f
         circularProgressDrawable.start()
 
-        var snackbar: Snackbar? = null
+        internetConnectionState.y = -internetConnectionState.height.toFloat()
 
         viewModel.currentSong.observe(viewLifecycleOwner, Observer { song ->
-
             title.text = song.title
             artist.text = song.artist
             if (!song.lyrics.isNullOrBlank()) {
@@ -78,8 +88,8 @@ class SpotifyLyricsFragment : Fragment() {
                 addTarget(mainLayout)
                 duration = 600
             }
-            TransitionManager.beginDelayedTransition(scrollView, transition)
 
+            TransitionManager.beginDelayedTransition(scrollView, transition)
             if (description != null) {
                 loadingLayout.visibility = View.VISIBLE
                 loadingDescription.text = description
@@ -94,7 +104,7 @@ class SpotifyLyricsFragment : Fragment() {
             if (shouldShowSnackbar) {
                 snackbar = Snackbar
                     .make(
-                        rootLayout,
+                        scrollView,
                         getString(R.string.check_connection),
                         Snackbar.LENGTH_INDEFINITE
                     )
@@ -103,6 +113,7 @@ class SpotifyLyricsFragment : Fragment() {
                         viewModel.refreshSong()
                     }
                 snackbar?.show()
+                return@Observer
             }
             if (!shouldShowSnackbar && snackbar?.isShown == true) {
                 Log.d("SpotifyLyricsFragment", "Dismissing")
@@ -121,10 +132,50 @@ class SpotifyLyricsFragment : Fragment() {
                             Toast.LENGTH_LONG
                         ).show()
                     is UserNotAuthorizedException -> showAuthorizationFailedDialog()
+                    is AuthenticationFailedException -> Toast.makeText(
+                        requireActivity(),
+                        "Connecting to Spotify has failed",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
         })
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            connection().collect { connected ->
+                if (connected) {
+                    internetConnectionState.animate()
+                        .translationY(-internetConnectionState.height.toFloat())
+                } else {
+                    internetConnectionState.animate().translationY(0F)
+                }
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun connection() = callbackFlow {
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if (intent.action == ConnectivityManager.CONNECTIVITY_ACTION) {
+                    offer(isOnline(context))
+                }
+            }
+
+            private fun isOnline(context: Context): Boolean {
+                val connectivityManager =
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                val networkInfo = connectivityManager.activeNetworkInfo
+                return networkInfo != null && networkInfo.isConnected
+            }
+        }
+        requireContext().registerReceiver(
+            receiver,
+            IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        )
+        awaitClose {
+            requireContext().unregisterReceiver(receiver)
+        }
     }
 
     private fun getSpannableStringWithBackground(
